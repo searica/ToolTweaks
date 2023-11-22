311b4759a59f86f022bd1bc3f518e7437c326d4e
searica <143636061+searica@users.noreply.github.com>
2023-11-22 15:24:32 -0800

    Static class optimizations

--- ToolTweaks.cs
+++ ToolTweaks.cs
@@ -17,7 +17,7 @@ namespace ToolTweaks
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     [BepInDependency(Jotunn.Main.ModGuid, Jotunn.Main.Version)]
     [NetworkCompatibility(CompatibilityLevel.VersionCheckOnly, VersionStrictness.Patch)]
-    internal class ToolTweaks : BaseUnityPlugin
+    internal sealed class ToolTweaks : BaseUnityPlugin
     {
         internal const string Author = "Searica";
         public const string PluginName = "ToolTweaks";
@@ -126,7 +126,7 @@ namespace ToolTweaks
     }
 
     [HarmonyPatch(typeof(Player))]
-    internal class PlayerPatch
+    internal static class PlayerPatch
     {
         private static bool UsingBuildTool = false;
 
@@ -221,28 +221,28 @@ namespace ToolTweaks
 
         #endregion Verbosity
 
-        internal static ManualLogSource _logSource;
+        private static ManualLogSource logSource;
 
         internal static void Init(ManualLogSource logSource)
         {
-            _logSource = logSource;
+            Log.logSource = logSource;
         }
 
-        internal static void LogDebug(object data) => _logSource.LogDebug(data);
+        internal static void LogDebug(object data) => logSource.LogDebug(data);
 
-        internal static void LogError(object data) => _logSource.LogError(data);
+        internal static void LogError(object data) => logSource.LogError(data);
 
-        internal static void LogFatal(object data) => _logSource.LogFatal(data);
+        internal static void LogFatal(object data) => logSource.LogFatal(data);
 
-        internal static void LogMessage(object data) => _logSource.LogMessage(data);
+        internal static void LogMessage(object data) => logSource.LogMessage(data);
 
-        internal static void LogWarning(object data) => _logSource.LogWarning(data);
+        internal static void LogWarning(object data) => logSource.LogWarning(data);
 
         internal static void LogInfo(object data, LogLevel level = LogLevel.Low)
         {
             if (Verbosity is null || VerbosityLevel >= level)
             {
-                _logSource.LogInfo(data);
+                logSource.LogInfo(data);
             }
         }
 
