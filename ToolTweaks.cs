7c6ee9c37a050dae7e60de7695511df278160e41
searica <143636061+searica@users.noreply.github.com>
2024-12-07 09:31:59 -0800

    Update: Bump version

--- ToolTweaks.cs
+++ ToolTweaks.cs
@@ -23,7 +23,7 @@ namespace ToolTweaks
         internal const string Author = "Searica";
         public const string PluginName = "ToolTweaks";
         public const string PluginGUID = $"{Author}.Valheim.{PluginName}";
-        public const string PluginVersion = "1.2.0";
+        public const string PluginVersion = "1.2.1";
 
         // Use this class to add your own localization to the game
         // https://valheim-modding.github.io/Jotunn/tutorials/localization.html
